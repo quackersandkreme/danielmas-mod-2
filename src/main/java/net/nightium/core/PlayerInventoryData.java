e626cff816e6ec1e9b5c9b609af6f33e52a8b277
quackersnkreme <charliefielding160@gmail.com>
2025-11-16 00:58:02 +1030

    fix(PlayerInventoryData.java): Updating functions to how inventory is handled in current version 1.21.10

--- src/main/java/net/nightium/core/PlayerInventoryData.java
+++ src/main/java/net/nightium/core/PlayerInventoryData.java
@@ -8,16 +8,16 @@ import net.minecraft.util.collection.DefaultedList;
 public class PlayerInventoryData {
     private final ItemStack[] main;
     private final ItemStack[] armor;
-    private final ItemStack[] offhand;
+    private final ItemStack offhand;
     private final int experienceLevel;
     private final float experienceProgress;
     private final int hunger;
     private final float saturation;
 
     public PlayerInventoryData(ServerPlayerEntity player) {
-        this.main = copyInventory(player.getInventory().main);
-        this.armor = copyInventory(player.getInventory().armor);
-        this.offhand = copyInventory(player.getInventory().offHand);
+        this.main = copyMainInventory(player.getInventory().getMainStacks());
+        this.armor = copyArmourInventory(player.getInventory().getStack(36), player.getInventory().getStack(37), player.getInventory().getStack(38), player.getInventory().getStack(39));
+        this.offhand = copyOffhandInventory(player.getInventory().getStack(40));
         this.experienceLevel = player.experienceLevel;
         this.experienceProgress = player.experienceProgress;
 
@@ -26,7 +26,7 @@ public class PlayerInventoryData {
         this.saturation = hungerManager.getSaturationLevel();
     }
 
-    private ItemStack[] copyInventory(DefaultedList<ItemStack> inventory) {
+    private ItemStack[] copyMainInventory(DefaultedList<ItemStack> inventory) {
         ItemStack[] copy = new ItemStack[inventory.size()];
         for (int i = 0; i < copy.length; i++) {
             copy[i] = inventory.get(i).copy();
@@ -34,10 +34,34 @@ public class PlayerInventoryData {
         return copy;
     }
 
+    private ItemStack[] copyArmourInventory(ItemStack feet, ItemStack legs, ItemStack chest, ItemStack head) {
+        ItemStack[] copy = new ItemStack[4];
+
+        copy[0] = feet.copy();
+        copy[1] = legs.copy();
+        copy[2] = chest.copy();
+        copy[3] = head.copy();
+
+        return copy;
+    }
+
+    private ItemStack copyOffhandInventory(ItemStack inventory) {
+        ItemStack copy;
+
+        copy = inventory.copy();
+
+        return copy;
+    }
+
     public void applyTo(ServerPlayerEntity player) {
-        applyInventory(player.getInventory().main, main);
-        applyInventory(player.getInventory().armor, armor);
-        applyInventory(player.getInventory().offHand, offhand);
+        applyInventory(player.getInventory().getMainStacks(), main);
+
+
+        for (int i = 0; i < armor.length; i++) {
+            player.getInventory().setStack(36+i, armor[i]);
+        }
+
+        player.getInventory().setStack(40, offhand);
 
         player.experienceLevel = this.experienceLevel;
         player.experienceProgress = this.experienceProgress;
@@ -54,4 +78,5 @@ public class PlayerInventoryData {
             target.set(i, source[i].copy());
         }
     }
+
 }
